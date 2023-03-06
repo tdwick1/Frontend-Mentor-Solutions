b51a0d04cc8338e2e150106dcd01af89faa5ff6e
tdwick1 <89940454+tdwick1@users.noreply.github.com>
2023-03-06 15:22:58 -0500

    Update app.js

--- advice-generator-app-main/app.js
+++ advice-generator-app-main/app.js
@@ -1,44 +1,101 @@
-const apiHost = "https://api.adviceslip.com/advice";
-const newAdvice = document.querySelector("#newAdvice");
+/* import font */
+@import url(https://fonts.google.com/specimen/Manrope);
 
-let db = false;
-let interval;
-let adviceNum = localStorage.getItem("adviceNum") || 0;
-
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(ms, resolve));
+* {
+  box-sizing: border-box;
+  margin: 0;
 }
 
-async function fetchAdvice() {
-  if (db) return;
-  db = true;
-  document.querySelector("#adviceText").innerText = "";
-  await fetch(apiHost)
-    .then((response) => response.json())
-    .then(async (data) => {
-      adviceNum++;
-      localStorage.setItem("adviceNum", adviceNum);
-      document.querySelector("#adviceId").textContent = `ADVICE #${adviceNum}`;
-      const advice = '"' + data.slip.advice + '"';
-      let curr = 0;
-      if (interval) {
-        document.querySelector("#adviceText").textContent = "";
-        clearInterval(interval);
-        interval = null;
-      }
-      interval = setInterval(() => {
-        if (curr == advice.length) {
-          clearInterval(interval);
-          interval = undefined;
-          return;
-        }
-        let letter = advice[curr];
-        document.querySelector("#adviceText").textContent += letter;
-        curr += 1;
-      }, 25);
-    });
-  db = false;
+body {
+  background-color: hsl(218, 23%, 16%);
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+
+  color: hsl(193, 38%, 86%);
+  font-family: "Manrope", sans-serif;
+  font-weight: 800;
 }
 
-fetchAdvice();
-newAdvice.addEventListener("click", fetchAdvice);
+.adviceCard {
+  background-color: hsl(217, 19%, 24%);
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  max-width: 380px;
+  max-height: 500px;
+  justify-content: center;
+  margin: 0 auto;
+  text-align: center;
+  margin-bottom: 15px;
+  padding: 2rem;
+  transition: max-height 0.15s ease-out;
+  position: relative;
+}
+
+@media (max-width: 500px) {
+  .adviceCard {
+    max-width: 75vw;
+  }
+}
+
+#adviceText {
+  font-size: 28px;
+  position: relative;
+}
+
+#adviceText::before {
+  content: "“";
+  font-size: 32px;
+  color: hsl(150, 100%, 66%);
+  position: absolute;
+  top: -10px;
+  left: -15px;
+}
+#adviceText::after {
+  content: "”";
+  font-size: 32px;
+  color: hsl(150, 100%, 66%);
+  position: absolute;
+  bottom: -10px;
+  right: -15px;
+}
+
+.adviceCard > * {
+  margin: 1rem;
+}
+
+.adviceCard #adviceId {
+  color: hsl(150, 100%, 66%);
+  font-weight: 400;
+  letter-spacing: 3px;
+  font-size: 12px;
+}
+
+.adviceCard #newAdvice {
+  all: unset;
+  cursor: pointer;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: -25px;
+  position: absolute;
+  background-color: hsl(150, 100%, 66%);
+  width: 3rem;
+  height: 3rem;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
+}
+.adviceCard #newAdvice:hover {
+  background-color: hsl(150, 100%, 66%);
+  opacity: 0.8;
+}
+
+.adviceCard #newAdvice img {
+  width: 1.5rem;
+  height: 1.5rem;
+}
